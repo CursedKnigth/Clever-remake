72998ab3c949bd23d2f60fe4a0cbbdfe18879659
CursedKnigth <128488578+CursedKnigth@users.noreply.github.com>
2025-10-03 14:18:49 +0300

    partially finished the dice box

--- dicebox.py
+++ dicebox.py
@@ -1,18 +1,42 @@
 import pygame
+import random
+from button import *
 
 class DiceBox():
     def __init__(self, screen):
         self.screen = screen
         self.x1 = 5
         self.y1 = 5
-        self.w = screen.get_width() / 11 * 5 - 5
+        self.w = screen.get_width() / 11 * 5 - self.x1
         self.h = 115
         self.x2 = screen.get_width() / 11 * 6
         self.y2 = 5
         self.active_player = 1
-        self.active_dice = [[0, "yellow"], [0, "blue"], [0, "green"], [0, "purple"], [0, "orange"]]
+        self.picked_dice = 0
+        self.reset_dice()
 
     def draw(self):
-        d_width = len(self.active_dice)*120
+        dice_box_width = len(self.active_dice)*120
         if(self.active_player==1):
+            a = 1
             
+    def roll_dice(self):
+        tem = []
+        for i in self.active_dice:
+            tem.append([random.randint(1, 6), i[1]])
+        self.active_dice = sorted(tem, reverse=1)
+    
+    def reset_dice(self):
+        self.active_dice = [[0, "yellow"], [0, "blue"], [0, "green"], [0, "purple"], [0, "orange"]]
+        self.roll_dice()
+    
+    def get_picked_dice(self):
+        return self.picked_dice 
+    
+    def pick_dice(self, dice):
+        self.picked_dice = dice
+    
+    def confirm_pick(self):
+        i = self.active_dice.find(self.picked_dice)
+        self.active_dice = self.active_dice[0:i]
+        self.picked_dice = 0
